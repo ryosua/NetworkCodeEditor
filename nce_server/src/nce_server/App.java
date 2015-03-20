86183e0007cb4f62e2038f1cf274d79f5ae65554
Ryan Yosua <rcy5017@psu.edu>
2015-03-19 22:25:40 -0400

    Started server netbeans project.

---
+++ nce_server/src/nce_server/App.java
@@ -0,0 +1,12 @@
+package nce_server;
+
+public class App
+{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+       System.out.println("Server App Started"); 
+    }
+}
