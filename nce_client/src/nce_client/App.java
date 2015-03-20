c756d6fb1f95614b2b9405c16c0f7ccb47e7d7b1
Ryan Yosua <rcy5017@psu.edu>
2015-03-19 22:21:03 -0400

    Started client net beans project.

---
+++ nce_client/src/nce_client/App.java
@@ -0,0 +1,12 @@
+package nce_client;
+
+public class App
+{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+       System.out.println("Client App Started"); 
+    }
+}
