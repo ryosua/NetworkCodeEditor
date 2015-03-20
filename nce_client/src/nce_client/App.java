89bc422176b83aa68274dc77b562e6e234dd0fa9
Ryan Yosua <rcy5017@psu.edu>
2015-03-19 22:53:15 -0400

    Got started on user interface.

--- nce_client/src/nce_client/App.java
+++ nce_client/src/nce_client/App.java
@@ -7,6 +7,7 @@ public class App
      */
     public static void main(String[] args)
     {
-       System.out.println("Client App Started"); 
+       System.out.println("Client App Started");
+       new Frame();
     }
 }
